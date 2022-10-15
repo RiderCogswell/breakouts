d8444d8d6d2de4d7cf41896db6f4f0c9eb666e3d
Ridercogswell <ridercogswell@gmail.com>
2022-10-15 13:48:33 -0700

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const students = [
+  "Carter Matschek",
+  "Haylee Lovelady",
+  "Heather Korczynski",
+  "Janvier Barreto Acevedo",
+  "John Wood",
+  "Jordan Barringer",
+  "Maria Ramirez",
+  "Melina Morales",
+  "Shawna White",
+  "Skipper Thurman",
+  "Tim Fleck",
+  "Wahee Chowdhury",
+  "Ye Kuang"
+];
+const student = students[Math.floor(Math.random() * students.length)];
+console.log(`The Leader Is: ${student}`);
